cca0f412b6ed5c3b4e9691d5f2af6baa558bce91
Josip Klaric <josip@querystorm.com>
2020-06-07 12:53:49 +0200

    Preparing solution

---
+++ AsyncDownloader/ActionType.cs
@@ -0,0 +1,9 @@
+﻿namespace AsyncDownloader
+{
+    public enum ActionType
+    {
+        Undefined = 0,
+        Download = 1,
+        Delete = 2
+    }
+}
---
+++ AsyncDownloader/DownloadAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncDownloader
+{
+    public class DownloadAction
+    {
+        public DownloadAction(string fileToDownload, string fileRepository)
+        {
+
+        }
+
+        public ActionType ActionKind { get; set; }
+
+        public string FileToDownload { get; set; }
+
+        public string FileRepository { get; set; }
+
+
+    }
+}
